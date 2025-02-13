7c22ec797a5de61269249d513c8d210dc349e0b4
HeshamM-088 <heshamkhalil1988@gmail.com>
2025-02-13 21:21:26 +0200

    s

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="w-full flex bg-orange-400 text-white flex-col justify-center items-center gap-[4em]">
-      <h1>Hesham</h1>
+      <h1>Heshamm</h1>
     </div>
   );
 };
