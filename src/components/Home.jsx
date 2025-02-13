c09fbbcfec6da35936d486958fb34ab10d4d7ca4
HeshamM-088 <heshamkhalil1988@gmail.com>
2025-02-13 20:05:28 +0200

    home com updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-full flex bg-orange-400 text-white flex-col justify-center items-center gap-[4em]">
+      <h1>Hesham</h1>
+    </div>
+  );
 };
 
 export default Home;
